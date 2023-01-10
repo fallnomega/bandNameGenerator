f0d37dde4edc5549a272ee5964b822fed88e7935
fallnomega <fallnomega@gmail.com>
2023-01-10 14:29:22 -0800

    learning

---
+++ bandNameGenerator.py
@@ -0,0 +1,10 @@
+
+
+print("Welcome to the Band Name Generator.")
+birthplace = input("Whats the name of the city you grew up in ?\n>")
+petsname = input("What's your pet's name ?\n>")
+
+
+print("Your band name is %s %s" % (birthplace,petsname))
+
+
